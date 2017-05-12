666cbafd8e8d5ab327696b57f99226fa3a2b2102
古娜 <shiti08@126.com>
2017-05-12 14:37:03 +0800

    Initial commit

---
+++ src/main/java/com/qingzhenyun/QingzhenyunUserFilemanagerApplication.java
@@ -0,0 +1,12 @@
+package com.qingzhenyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingzhenyunUserFilemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QingzhenyunUserFilemanagerApplication.class, args);
+	}
+}

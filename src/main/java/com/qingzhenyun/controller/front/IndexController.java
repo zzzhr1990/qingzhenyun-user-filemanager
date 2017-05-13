d566938b549ce6fc304dea062922aa5258c285fe
古娜 <shiti08@126.com>
2017-05-13 13:58:04 +0800

    Try To save files

---
+++ src/main/java/com/qingzhenyun/controller/front/IndexController.java
@@ -0,0 +1,21 @@
+package com.qingzhenyun.controller.front;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * IndexController
+ * Created by guna on 2017/5/13.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping({"/", ""})
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping({"/", ""})
+    public String hello() {
+        return "hello";
+    }
+}
